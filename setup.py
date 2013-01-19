e38a40a6be873f4e1e882e4aeab227a56c9fdf2f
Sean Murray <smurray@novacoast.com>
2013-01-19 14:22:56 -0800

    accidentally added pycs, damnit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Neith',
+    version='0.1dev',
+    packages=['neith',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
